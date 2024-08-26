1720799bc1b5c7b1d06ddbd2350a643166840635
mubin <mubin.ice.ru@gmail.com>
2024-08-27 00:46:45 +0600

    get product by category

--- app/categories/[id]/page.tsx
+++ app/categories/[id]/page.tsx
@@ -1,7 +1,14 @@
-export default function Page({props}:any){
+import ProductCard from "@/components/ProductCard";
+import { categoryProduct } from "@/constants/products";
+
+export default function Page({ props }: any) {
     return (
-        <div>
-            {props?.id}
+        <div className="w-full">
+            <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5 my-4 mx-12">
+                {categoryProduct.map((item, idx) => (
+                    <ProductCard key={idx} product={item} />
+                ))}
+            </div>
         </div>
     )
 }
---
+++ lib/date.ts
@@ -0,0 +1,12 @@
+export function getTimeDifference(targetTime:string) {
+    const now:any = new Date();
+    const targetDate:any = new Date(targetTime);
+  
+    const timeDiff = targetDate - now;
+  
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  
+    return { days, hours, minutes };
+  }
